c9c0e70048c8fcde920d13f0a86538fc2355a3fa
Mr Nice Guy <67551080+MrNiceGuy9@users.noreply.github.com>
2023-09-01 19:37:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -71,55 +71,7 @@ if (overlay) {
 }
 
 
-document.addEventListener("DOMContentLoaded", function () {
-  var slideIndex = 1;
-  showDivs(slideIndex);
 
-  function plusDivs(n) {
-    showDivs(slideIndex += n);
-  }
-
-
-
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("myMaps");
-    if (x.length === 0) return; // Check if there are no elements with class "myMaps"
-    
-    if (n > x.length) { slideIndex = 1; }
-    if (n < 1) { slideIndex = x.length; }
-    
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    
-    x[slideIndex - 1].style.display = "block";
-  }
-})
-
-
-
-var slideIndex2 = 1;
-showDivs2(slideIndex2);
-
-function plusDivs2(n) {
-  showDivs2(slideIndex2 += n);
-}
-
-function showDivs2(n) {
-  var i;
-  var x = document.getElementsByClassName("myQuotes");
-  if (x.length === 0) return;
-  
-  if (n > x.length) { slideIndex2 = 1; }
-  if (n < 1) { slideIndex2 = x.length; }
-  
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  
-  x[slideIndex2 - 1].style.display = "block";
-}
 
 var elements = document.getElementsByClassName("game-mode1");
 
